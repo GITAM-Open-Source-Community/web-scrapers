0ccb42d1496833321ec4897adfc45492d5489ef4
Ritesh Maurya <31176772+MauryaRitesh@users.noreply.github.com>
2021-10-15 09:14:48 +0530

    Update main.py

--- Python/get-github-topic/main.py
+++ Python/get-github-topic/main.py
@@ -9,5 +9,16 @@ soup = BeautifulSoup(res.text, 'lxml')
 
 repos = soup.find_all('h3', {'class': 'f3 color-text-secondary text-normal lh-condensed'})
 
-for repo in repos:
+#Default Descending order
+'''for repo in repos:
     print("https://github.com" + repo.find_all('a')[1]['href'])
+'''
+    
+#For Ascending Order(Starting with the least number of stars and save the list in a text file)
+
+textfile = open("github_topics.txt", "w")
+for repo in reversed(repos):
+    print("https://github.com" + repo.find_all('a')[1]['href'])
+    textfile.write(repo + "\n")
+
+textfile.close()
