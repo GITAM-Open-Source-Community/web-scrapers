b7db503cd9e9d914d3d5c9c1d9f49eada7a47589
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-02-11 00:40:22 +0530

    bug fix
    - Python/lyrics had a bug where it showed only some lyrics for some songs

--- Python/get-lyrics/main.py
+++ Python/get-lyrics/main.py
@@ -1,18 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = {
-    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
-}
 
 def getLyrics(title):
+    headers = {
+        "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
+    }
     session = requests.Session()
     res = session.get("https://www.musixmatch.com/search/"+title, headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
     link = soup.find(class_ = "title").attrs["href"]
     res = session.get('https://www.musixmatch.com/'+link, headers=headers)
     soup = BeautifulSoup(res.text, 'html.parser')
-    lyrics = soup.find(class_ = "lyrics__content__ok").getText()
+    lyrics=""
+    for i in soup.find_all(class_ = "lyrics__content__ok"):
+        lyrics += i.text+'\n'
     return lyrics
 
 lyrics = getLyrics("Treat you better")
